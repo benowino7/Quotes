db22e2ca64e25f54bd054454a2969b03a2f37069
benson7-coder <bensonowino7@gmail.com>
2020-01-12 21:31:42 +0300

    added funcions that likes and dislikes, function to delete and styles the respective html files

---
+++ src/app/quote-like.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { QuoteLikePipe } from './quote-like.pipe';
+
+describe('QuoteLikePipe', () => {
+  it('create an instance', () => {
+    const pipe = new QuoteLikePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
