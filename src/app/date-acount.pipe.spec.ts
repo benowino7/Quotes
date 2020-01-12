16825c10d9c1eb09c0302d892ee06f6ddaca53ef
benson7-coder <bensonowino7@gmail.com>
2020-01-12 08:37:24 +0300

    created css, html, json, ajax, js, md, jquery and bootstrap files

---
+++ src/app/date-acount.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DateCountPipe } from './date-acount.pipe';
+
+describe('DateAcountPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DateCountPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
