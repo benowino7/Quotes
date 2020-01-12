670c410fd6f53924c8f29503d83728b5e92dabac
benson7-coder <bensonowino7@gmail.com>
2020-01-12 21:27:47 +0300

    added funcions that likes and dislikes, function to delete and styles the respective html files

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -18,6 +18,22 @@ export class QuoteDetailComponent implements OnInit {
   quoteDelete(complete:boolean){
     this.isComplete.emit(complete);
   }
+  likeQuo=()=>{
+      document.getElementById('dislike').addEventListener('click',()=>{like(1);});
+document.getElementById('display').addEventListener('click',()=>{like(0);});
+function like(lk) 
+{
+var likes, dlikes;
+
+
+ likes=parseInt(document.getElementById('lk').innerText);
+ dlikes=parseInt(document.getElementById('dlk').innerText);
+ if(lk==1){ dlikes++;} else{ likes=likes; likes++;}
+ if(likes<=0){likes=0;} if(dlikes<=0){dlikes=0;}
+ document.getElementById('lk').innerText=likes;
+ document.getElementById('dlk').innerText=dlikes;
+ }
+  }
   
   ngOnInit() {
   }
